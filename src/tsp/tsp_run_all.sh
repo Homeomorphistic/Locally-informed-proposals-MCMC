0cb0517e9512cd312d03c0f019d6892d31968998
Homeomorphistic <bartex078@tlen.pl>
2022-05-20 12:16:49 +0200

    Added turning off after script and new results for sprint-1

--- src/tsp/tsp_run_all.sh
+++ src/tsp/tsp_run_all.sh
@@ -2,9 +2,10 @@
 # Script used to run all the traveling salesmen problems with all combinations of parameters.
 
 SAVE=${1:-"False"}
+TURNOFF=${2:-"False"}
 LOCALLY_LIST="False True"
 TOLERANCE_LIST="0.01"
-MAX_ITER_LIST="1000 2000"
+MAX_ITER_LIST="20000 25000"
 
 for MAX_ITER in $MAX_ITER_LIST
   do
@@ -16,3 +17,12 @@ for MAX_ITER in $MAX_ITER_LIST
           done
       done
   done
+
+# Shutdown if finished at night.
+if [ "$TURNOFF" = "True" ];
+then
+  if ! ((8<=$(date +%-H) && $(date +%-H)<23));
+  then
+      echo shutdown
+  fi
+fi
